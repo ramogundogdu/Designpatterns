f770e917999ddfcc1dcb5fbe2aaf3f46663b5784
ramogundogdu <ramazan.guendogdu@hs.furtwangen.de>
2014-11-16 21:31:12 +0100

    Singleton Pattern

---
+++ DesignPattern/SingletonDesignPattern/SingletonDesignPackeges/SingletonPatternDemo.java
@@ -0,0 +1,14 @@
+package SingletonDesignPackeges;
+
+public class SingletonPatternDemo {
+	
+	public static void main(String[] args) {
+		
+		//Get the only object available
+		SingleObject object = SingleObject.getInstance();
+		
+		//show the message
+		object.showMessage();
+	}
+
+}
