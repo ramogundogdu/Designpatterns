ed1037a9f798f0f2578eda40a73b13cdf46a99d1
ramogundogdu <ramazan.guendogdu@hs.furtwangen.de>
2014-11-16 22:54:01 +0100

    MealBuilder added

---
+++ DesignPattern/BuilderDesignPattern/BuilderDesignPatternPackages/Pepsi.java
@@ -0,0 +1,15 @@
+package BuilderDesignPatternPackages;
+
+public class Pepsi extends ColdDrink{
+	
+	@Override
+	public float price(){
+		return 35.5f;
+	}
+
+	@Override
+	public String name() {
+		return "Pepsi";
+	}
+
+}
